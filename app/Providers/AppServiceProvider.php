309b3cadd0e34bdb1056aa2c2d496c3f1792953a
Dambi Stuart <31619775+StuartArth@users.noreply.github.com>
2018-09-12 11:43:30 -0700

    The Automatic Timetable Generator

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //this increases the default number of characters to be entered to the databse 
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
